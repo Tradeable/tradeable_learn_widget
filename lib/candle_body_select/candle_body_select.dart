b0d172a68de333e61f3bcfb4de1b565e460627ba
deepakgrandhi <deepakgrandhi@gmail.com>
2024-09-25 11:07:31 +0530

    updated candle body select widget

--- lib/candle_body_select/candle_body_select.dart
+++ lib/candle_body_select/candle_body_select.dart
@@ -23,141 +23,98 @@ class _CandleBodySelectState extends State<CandleBodySelect> {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).customColors;
+    final theme = Theme.of(context);
+    final colors = theme.customColors;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        renderQuestion(),
-        Center(child: renderSingleSelectQuestion(model.userResponse ?? "")),
+        renderQuestion(theme),
+        Center(
+            child:
+                renderSingleSelectQuestion(model.userResponse ?? "", colors)),
         const Spacer(),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
           child: ButtonWidget(
             color: colors.primary,
             btnContent: "Submit",
-            onTap: () {
-              submitResponse();
-            },
+            onTap: submitResponse,
           ),
         ),
       ],
     );
   }
 
-  Widget renderQuestion() {
-    final textStyles = Theme.of(context).customTextStyles;
+  Widget renderQuestion(ThemeData theme) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Text(
         model.question,
-        style: textStyles.smallNormal,
+        style: theme.customTextStyles.smallNormal,
       ),
     );
   }
 
-  Widget renderSingleSelectQuestion(String singleSelectAns) {
-    final colors = Theme.of(context).customColors;
-
-    Color candleColor =
+  Widget renderSingleSelectQuestion(
+      String selectedAnswer, CustomColors colors) {
+    final candleColor =
         model.isBullish ? colors.bullishColor : colors.bearishColor;
-    String correctResponse = "";
-    switch (model.state) {
-      case CandleBodySelectState.loadUI:
-        correctResponse = "";
-        break;
-      case CandleBodySelectState.submitResponse:
-        correctResponse = model.correctResponse;
-        break;
-    }
+    final correctResponse = model.state == CandleBodySelectState.submitResponse
+        ? model.correctResponse
+        : "";
+
     return Container(
-        margin: const EdgeInsets.only(top: 30),
-        height: 400,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+      margin: const EdgeInsets.only(top: 30),
+      height: 400,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          for (var part in candleParts())
             CandleBodyPart(
-              type: "high",
-              height: model.wickHeight * 0.4,
-              width: 10,
+              type: part['type'],
+              height: part['height'],
+              width: part['width'],
               color: candleColor,
-              currentSelected: singleSelectAns,
+              currentSelected: selectedAnswer,
               correctResponse: correctResponse,
               onTap: showAnimation,
             ),
-            CandleBodyPart(
-              type: "wick",
-              height: model.wickHeight * 0.8,
-              width: 10,
-              color: candleColor,
-              currentSelected: singleSelectAns,
-              correctResponse: correctResponse,
-              onTap: showAnimation,
-            ),
-            CandleBodyPart(
-              type: model.isBullish ? "close" : "open",
-              height: model.bodyHeight * 0.2,
-              width: 40,
-              color: candleColor,
-              currentSelected: singleSelectAns,
-              correctResponse: correctResponse,
-              onTap: showAnimation,
-            ),
-            CandleBodyPart(
-              type: "body",
-              height: model.bodyHeight * 0.6,
-              width: 40,
-              color: candleColor,
-              currentSelected: singleSelectAns,
-              correctResponse: correctResponse,
-              onTap: showAnimation,
-            ),
-            CandleBodyPart(
-              type: model.isBullish ? "open" : "close",
-              height: model.bodyHeight * 0.2,
-              width: 40,
-              color: candleColor,
-              currentSelected: singleSelectAns,
-              correctResponse: correctResponse,
-              onTap: showAnimation,
-            ),
-            CandleBodyPart(
-              type: "tail",
-              height: model.tailHeight * 0.8,
-              width: 10,
-              color: candleColor,
-              currentSelected: singleSelectAns,
-              correctResponse: correctResponse,
-              onTap: showAnimation,
-            ),
-            CandleBodyPart(
-              type: "low",
-              height: model.tailHeight * 0.4,
-              width: 10,
-              color: candleColor,
-              currentSelected: singleSelectAns,
-              correctResponse: correctResponse,
-              onTap: showAnimation,
-            ),
-          ],
-        ));
+        ],
+      ),
+    );
   }
 
-  void showAnimation(String response) async {
+  List<Map<String, dynamic>> candleParts() {
+    return [
+      {"type": "high", "height": model.wickHeight * 0.4, "width": 10},
+      {"type": "wick", "height": model.wickHeight * 0.8, "width": 10},
+      {
+        "type": model.isBullish ? "close" : "open",
+        "height": model.bodyHeight * 0.2,
+        "width": 40
+      },
+      {"type": "body", "height": model.bodyHeight * 0.6, "width": 40},
+      {
+        "type": model.isBullish ? "open" : "close",
+        "height": model.bodyHeight * 0.2,
+        "width": 40
+      },
+      {"type": "tail", "height": model.tailHeight * 0.8, "width": 10},
+      {"type": "low", "height": model.tailHeight * 0.4, "width": 10},
+    ];
+  }
+
+  void showAnimation(String response) {
     setState(() {
       model.userResponse = response;
-    });
-    if (model.correctResponse == model.userResponse) {
-      setState(() {
+      if (model.correctResponse == response) {
         model.isCorrect = true;
-      });
-    }
+      }
+    });
   }
 
   void submitResponse() {
-    final textStyles = Theme.of(context).customTextStyles;
-    final colors = Theme.of(context).customColors;
-
     if ((model.userResponse ?? "").isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
         content: Text("Tap on the candle to proceed"),
@@ -167,94 +124,123 @@ class _CandleBodySelectState extends State<CandleBodySelect> {
         model.state = CandleBodySelectState.submitResponse;
       });
       showModalBottomSheet(
-          isDismissible: false,
-          context: context,
-          builder: (context) => Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(12),
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(10),
-                        topLeft: Radius.circular(10))),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                          height: 300,
-                          width: double.infinity,
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 24, vertical: 10),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: model.isCorrect
-                                  ? colors.bullishColor.withOpacity(0.4)
-                                  : colors.bearishColor.withOpacity(0.4))),
-                      Text(model.isCorrect ? "Great!" : "Incorrect",
-                          style: textStyles.mediumBold),
-                      Text(
-                        "Explanation comes here",
-                        style: textStyles.smallNormal,
-                      ),
-                      const SizedBox(height: 10),
-                      ButtonWidget(
-                          color: colors.primary,
-                          btnContent: "Next",
-                          onTap: () {})
-                    ],
+        isDismissible: false,
+        context: context,
+        builder: (context) => buildBottomSheet(),
+      );
+    }
+  }
+
+  Widget buildBottomSheet() {
+    final theme = Theme.of(context);
+    final colors = theme.customColors;
+    final candleColor =
+        model.isBullish ? colors.bullishColor : colors.bearishColor;
+
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(12),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(10),
+          topLeft: Radius.circular(10),
+        ),
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: 300,
+                  width: double.infinity,
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: model.isCorrect
+                        ? colors.bullishColor.withOpacity(0.4)
+                        : colors.bearishColor.withOpacity(0.4),
                   ),
                 ),
-              ));
-    }
+                SizedBox(
+                    height: 300, child: buildCandleModel(colors, candleColor)),
+              ],
+            ),
+            Text(model.isCorrect ? "Great!" : "Incorrect",
+                style: theme.customTextStyles.mediumBold),
+            Text("Explanation comes here",
+                style: theme.customTextStyles.smallNormal),
+            const SizedBox(height: 10),
+            ButtonWidget(
+              color: colors.primary,
+              btnContent: "Next",
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildCandleModel(CustomColors colors, Color candleColor) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: candleParts()
+          .map((part) => Flexible(
+                child: CandleBodyPart(
+                  type: part['type'],
+                  height: part['height'],
+                  width: part['width'],
+                  color: candleColor,
+                  currentSelected: "",
+                  correctResponse: model.correctResponse,
+                  onTap: showAnimation,
+                ),
+              ))
+          .toList(),
+    );
   }
 }
 
 class CandleBodyPart extends StatelessWidget {
   final String type;
   final double height;
-  final double width;
+  final int width;
   final Color color;
   final String currentSelected;
   final String correctResponse;
   final Function(String) onTap;
 
-  const CandleBodyPart(
-      {super.key,
-      required this.type,
-      required this.height,
-      required this.width,
-      required this.color,
-      required this.currentSelected,
-      required this.correctResponse,
-      required this.onTap});
+  const CandleBodyPart({
+    super.key,
+    required this.type,
+    required this.height,
+    required this.width,
+    required this.color,
+    required this.currentSelected,
+    required this.correctResponse,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).customColors;
+    final isSelected = currentSelected == type;
 
     return InkWell(
-      onTap: () {
-        if (correctResponse == "") {
-          onTap(type);
-        }
-      },
+      onTap: correctResponse.isEmpty ? () => onTap(type) : null,
       child: Container(
         height: height,
-        width: width,
+        width: width.toDouble(),
         color: correctResponse == type
             ? colors.selectedItemColor
-            : currentSelected == type
+            : isSelected
                 ? colors.selectedItemColor.withOpacity(0.5)
                 : color,
       ),
     );
   }
-}
-
-class CandlePartSelectQuestion {
-  final String question;
-  final String answer;
-
-  CandlePartSelectQuestion(
-      {required String type, required this.question, required this.answer});
 }
