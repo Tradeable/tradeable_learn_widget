66a9b83c83549afb6b3f92f90c7ef9f63015bcf4
Aditya Gohad <adityagohad@Adityas-MacBook-Pro.local>
2024-10-17 06:14:17 +0530

    removed demo widget

--- lib/tradeable_learn_widget.dart
+++ lib/tradeable_learn_widget.dart
@@ -1,8 +1,5 @@
 library tradeable_learn_widget;
 
-export 'package:tradeable_learn_widget/demo_widget/demo_widget_model.dart';
-export 'package:tradeable_learn_widget/demo_widget/demo_widget.dart';
-
 export 'package:tradeable_learn_widget/ladder_widget/ladder_widget_main.dart';
 export 'package:tradeable_learn_widget/ladder_widget/ladder_data_model.dart';
 
