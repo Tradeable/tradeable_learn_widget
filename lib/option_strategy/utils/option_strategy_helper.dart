7ae80b92783221e3816c08ef4e9b3b300bf99b74
adityagohad <adityagohad@gmail.com>
2025-01-30 13:33:48 +0530

    safe side push

--- lib/option_strategy/utils/option_strategy_helper.dart
+++ lib/option_strategy/utils/option_strategy_helper.dart
@@ -8,6 +8,7 @@ class OptionStrategyHelper {
   final List<OptionLeg> legs;
   late List<double> xValues;
   late List<FlSpot> expirationPnLvalues;
+  late List<List<FlSpot>> expirationPnLSegments;
   late double xMin;
   late double xMax;
   late double yMin;
@@ -18,6 +19,7 @@ class OptionStrategyHelper {
     xMin = xValues.first;
     xMax = xValues.last;
     expirationPnLvalues = _calculateExpirationPnLFlSpots();
+    expirationPnLSegments = _calculateExpirationPnLSegments();
     yMin = expirationPnLvalues.map((e) => e.y).reduce(min);
     yMax = expirationPnLvalues.map((e) => e.y).reduce(max);
   }
@@ -140,5 +142,103 @@ class OptionStrategyHelper {
     }
 
     return flSpots;
+  }
+
+  List<List<FlSpot>> _calculateExpirationPnLSegments() {
+    List<FlSpot> allPoints = _calculateAllPnLPoints();
+    List<double> breakEvenPoints = _findBreakEvenPoints(allPoints);
+    return _splitIntoSegments(allPoints, breakEvenPoints);
+  }
+
+  List<FlSpot> _calculateAllPnLPoints() {
+    List<double> xAxisValues = xValues;
+    List<double> totalPnL = List<double>.filled(xAxisValues.length, 0);
+    List<FlSpot> flSpots = [];
+
+    for (OptionLeg leg in legs) {
+      List<double> legPnL = calculateSingleLegPnL(leg: leg);
+      for (int i = 0; i < legPnL.length; i++) {
+        totalPnL[i] += legPnL[i];
+      }
+    }
+
+    for (int i = 0; i < xAxisValues.length; i++) {
+      flSpots.add(FlSpot(xAxisValues[i], totalPnL[i]));
+    }
+
+    return flSpots;
+  }
+
+  List<double> _findBreakEvenPoints(List<FlSpot> pnlCurve) {
+    List<double> breakEvenPoints = [];
+
+    for (int i = 0; i < pnlCurve.length - 1; i++) {
+      double currentPnL = pnlCurve[i].y;
+      double nextPnL = pnlCurve[i + 1].y;
+
+      if ((currentPnL <= 0 && nextPnL >= 0) ||
+          (currentPnL >= 0 && nextPnL <= 0)) {
+        double x1 = pnlCurve[i].x;
+        double x2 = pnlCurve[i + 1].x;
+        double y1 = currentPnL;
+        double y2 = nextPnL;
+
+        double breakEven = x1 + (0 - y1) * (x2 - x1) / (y2 - y1);
+        breakEvenPoints.add(double.parse(breakEven.toStringAsFixed(2)));
+      }
+    }
+
+    return breakEvenPoints;
+  }
+
+  List<List<FlSpot>> _splitIntoSegments(
+      List<FlSpot> allPoints, List<double> breakEvenPoints) {
+    if (breakEvenPoints.isEmpty) {
+      return [
+        allPoints
+      ]; // Return all points as a single segment if no break-even points
+    }
+
+    List<List<FlSpot>> segments = [];
+    List<FlSpot> currentSegment = [];
+    int currentPointIndex = 0;
+
+    // Sort break-even points to ensure proper segmentation
+    breakEvenPoints.sort();
+
+    for (double breakEvenPoint in breakEvenPoints) {
+      currentSegment = [];
+
+      // Add points until we reach or pass the break-even point
+      while (currentPointIndex < allPoints.length &&
+          allPoints[currentPointIndex].x <= breakEvenPoint) {
+        currentSegment.add(allPoints[currentPointIndex]);
+        currentPointIndex++;
+      }
+
+      // Add interpolated point at break-even
+      if (currentPointIndex > 0 && currentPointIndex < allPoints.length) {
+        FlSpot prev = allPoints[currentPointIndex - 1];
+        FlSpot next = allPoints[currentPointIndex];
+
+        // Linear interpolation for y value at break-even point
+        double slope = (next.y - prev.y) / (next.x - prev.x);
+        double y = prev.y + slope * (breakEvenPoint - prev.x);
+
+        currentSegment.add(FlSpot(breakEvenPoint, y));
+      }
+
+      if (currentSegment.isNotEmpty) {
+        segments.add(currentSegment);
+      }
+    }
+
+    // Add remaining points as the final segment
+    if (currentPointIndex < allPoints.length) {
+      currentSegment = allPoints.sublist(currentPointIndex);
+      segments.add(currentSegment);
+    }
+
+    return segments;
   }
 }
